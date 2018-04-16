1fb3fc5b96ffd03ba4664ed16ce1402a9d6264cc
xialvjun <xialvjun@live.com>
2018-04-17 02:38:43 +0800

    v0.0.2: get_path and set_path

---
+++ src/get_path.ts
@@ -0,0 +1,29 @@
+/**
+ * 简易版的 lodash.get 。。。因为有了个自己写的 set_path，对称？
+ * @param obj 
+ * @param path 
+ * @param default_value 
+ */
+export function get_path(obj, path: string | (string | number)[], default_value?) {
+  if (obj === undefined) {
+    return default_value;
+  }
+  let paths = <(string | number)[]>path;
+  if (typeof path === 'string') {
+    paths = path.split(/[\.\[\]\'\"]/g).filter(v => v !== '').map(s => {
+      const idx = parseInt(s);
+      if ((idx > -1) && (idx + '' === s)) {
+        return idx;
+      }
+      return s;
+    });
+  }
+  if (paths.length === 0) {
+    return obj;
+  }
+  let head = undefined;
+  if (obj !== null) {
+    head = obj[paths[0]];
+  }
+  return get_path(head, paths.slice(1), default_value);
+}
--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,6 @@ export * from './args_parallel_limit';
 export * from './cache_pending_promise';
 export * from './cache_resolved_promise';
 export * from './channel';
+export * from './get_path';
 export * from './parallel_limit';
+export * from './set_path';
