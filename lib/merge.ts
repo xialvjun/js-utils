3eac138c45d3ad395a41346c76b2fd16907f2a68
xialvjun <xialvjun@live.com>
2020-07-17 19:22:51 +0800

    new ts lib structure

--- lib/merge.ts
+++ lib/merge.ts
@@ -3,6 +3,9 @@ import { to_string } from './to_string';
 const is_array = (obj: any) => to_string(obj) === '[object Array]';
 
 const merge2 = (a: any, b: any) => {
+  if (b === undefined) {
+    return a;
+  }
   // 如果 b 是 null 或 原始类型, 直接返回 b
   // a 必须是 数组 或者 非空对象, 否则返回 b
   if (
@@ -30,37 +33,7 @@ const merge2 = (a: any, b: any) => {
  * @param objs
  */
 export function merge(...objs: any[]) {
-  return objs.reduce((acc, cv) => merge2(acc, cv), undefined);
-  // objs = objs.filter((it) => typeof it === 'object' && it !== null);
-  // if (objs.length === 0) {
-  //   return undefined;
-  // }
-  // let res: any = is_array(objs[0]) ? [] : {...objs[0]};
-  // // const res: any = {};
-  // for (const it of objs) {
-  //   for (const key in it) {
-  //     if (Object.prototype.hasOwnProperty.call(it, key)) {
-  //       const value = it[key];
-  //       if (value === undefined) {
-  //         continue;
-  //       }
-  //       if (typeof value === 'object' && value !== null) {
-  //         res[key] = merge(res[key], value);
-  //       }
-  //       res[key] = value;
-  //     }
-  //   }
-  // }
-  // return res;
+  // return objs.reduce((acc, cv) => merge2(acc, cv), undefined);
+  // * better performance
+  return objs.reduceRight((acc, cv) => merge2(cv, acc), undefined);
 }
-
-// const defineProperties = (obj, props) => {
-//   const cew = { configurable: true, enumerable: true, writable: true };
-//   Object.defineProperties(
-//     obj,
-//     Object.keys(props).reduce((acc, key) => {
-//       acc[key] = { ...cew, value: props[key] };
-//       return acc;
-//     }, {}),
-//   );
-// };
